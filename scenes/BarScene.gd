02905c9f94c164c6d0235c7902395dd0ae88f78c
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2023-12-21 11:13:37 -0800

    setup for customer line

--- scenes/BarScene.gd
+++ scenes/BarScene.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+var Customers: Array
+@export var customer_scene: PackedScene
+var num_customers
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	num_customers = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if num_customers == 0:
+		pass
 
 
 func _on_go_to_cauldron_button_up():
