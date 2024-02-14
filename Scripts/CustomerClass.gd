27ef3c12eb7bba1408e5091c0307e2ee1edb9754
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-02-13 19:12:20 -0800

    randomize order :8ball:

---
+++ Scripts/CustomerClass.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name CustomerBase
+
+var carrots:bool = false
+var onions:bool = false
+#var order = [carrots, onions]
+var order:Dictionary = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	#for i in order.size():
+		#order[i] = randi_range(0, 1)
+		#print(order[i])
+		
+	order = {"carrots": (randtf()), 
+			"onions": (randtf())}
+
+	print("order is: ", order)
+
+func randtf():
+	if randi_range(0,1): return true 
+	else: return false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scenes/BarScene.gd
+++ scenes/BarScene.gd
@@ -6,20 +6,26 @@ var num_customers
 @onready var clock = get_node("/root/Clock")
 @onready var clock_display = get_node("UI/Clock")
 var myNode = preload("res://scenes/temp_customer.tscn")
+var customer1 = preload("res://prefabs/customer1.tscn")
+var customer2 = preload("res://prefabs/customer2.tscn")
 var orders = 0
 var instance
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#instantiates obj, can be made into method
-	instance = myNode.instantiate()
+	randomize()
+	var customers = [myNode, customer1, customer2]
+	var kinds = customers[randi() % customers.size()]
+	instance = kinds.instantiate()
+	#instance = myNode.instantiate()
 	instance.position = get_node("Customers/Ordering").position
 	add_child(instance)
+	
+	#randomize()
 	
 	num_customers = 0
 	if clock == null:
 		print("Clock Not Found")
-	else:
-		print("Clock!")
 		
 	if clock.get_child(0).time_left == 0: # restart timer if timed out
 		clock.get_child(0).start()
@@ -32,8 +38,6 @@ func _process(delta):
 	if num_customers == 0:
 		pass
 		
-	if instance.getScript().acceptOrder:
-		print("accepted")
 
 
 func _on_go_to_cauldron_button_up():
